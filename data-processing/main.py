472a4cc27b29e10041b43d31234f9a7d18ba55ca
nunawa <71294849+nunawa@users.noreply.github.com>
2025-09-21 00:24:04 +0900

    Add to aggregate Env WBGT

--- data-processing/main.py
+++ data-processing/main.py
@@ -7,6 +7,7 @@ from duckdb import (
     ColumnExpression,
     ConstantExpression,
     DuckDBPyRelation,
+    SQLExpression,
 )
 
 data_dir = Path(__file__).parents[1].joinpath("data")
@@ -151,12 +152,126 @@ def create_monthly_yearly_weather_object():
     return weather_data
 
 
+def create_daily_wbgt_object():
+    dirs = [x for x in env_wbgt_dir.glob("*") if x.is_dir()]
+
+    wbgt_data = {}
+    for dir in dirs:
+        # 5年 * 7ヶ月 = 35ファイルあるはず
+        if len(list(dir.glob("final_wbgt_*.csv"))) != 35:
+            continue
+
+        hourly = duckdb.read_csv(dir.joinpath("final_wbgt_*.csv")).set_alias("hourly")
+
+        min_max_by_date = (
+            hourly.select(
+                "Date",
+                "WBGT",
+            )
+            .aggregate(
+                aggr_expr=[
+                    "Date",
+                    SQLExpression("min(WBGT)").alias("min_wbgt"),
+                    SQLExpression("max(WBGT)").alias("max_wbgt"),
+                ],
+                group_expr="Date",
+            )
+            .order("Date")
+            .set_alias("min_max_by_date")
+        )
+
+        daily = (
+            hourly.join(min_max_by_date, condition="Date")
+            .select(
+                SQLExpression("month(Date)").alias("month"),
+                SQLExpression("day(Date)").alias("day"),
+                "WBGT",
+                "min_wbgt",
+                "max_wbgt",
+            )
+            .aggregate(
+                aggr_expr=[
+                    "month",
+                    "day",
+                    SQLExpression("round_even(avg(min_wbgt), 1)").alias("min_wbgt"),
+                    SQLExpression("round_even(avg(max_wbgt), 1)").alias("max_wbgt"),
+                    SQLExpression("round_even(avg(WBGT), 1)").alias("avg_wbgt"),
+                ],
+                group_expr="month, day",
+            )
+            .order("month, day")
+            .fetchall()
+        )
+
+        station_number = dir.stem
+        wbgt_data[station_number] = {
+            f"{d[0]}/{d[1]}": {
+                "min": d[2],
+                "max": d[3],
+                "avg": d[4],
+            }
+            for d in daily
+        }
+        print(f"Loaded station {station_number}")
+
+    return wbgt_data
+
+
+def create_monthly_yearly_wbgt_object():
+    dirs = [x for x in env_wbgt_dir.glob("*") if x.is_dir()]
+
+    wbgt_data = {}
+    for dir in dirs:
+        # 5年 * 7ヶ月 = 35ファイルあるはず
+        if len(list(dir.glob("final_wbgt_*.csv"))) != 35:
+            continue
+
+        hourly = duckdb.read_csv(dir.joinpath("final_wbgt_*.csv")).set_alias("hourly")
+
+        yearly = (
+            hourly.select("WBGT")
+            .aggregate([SQLExpression("round_even(avg(WBGT), 1)").alias("avg_wbgt")])
+            .fetchone()
+        )
+
+        monthly = (
+            hourly.select(
+                SQLExpression("month(Date)").alias("month"),
+                "WBGT",
+            )
+            .aggregate(
+                aggr_expr=[
+                    "month",
+                    SQLExpression("round_even(avg(WBGT), 1)").alias("avg_wbgt"),
+                ],
+                group_expr="month",
+            )
+            .order("month")
+            .fetchall()
+        )
+
+        station_number = dir.stem
+        wbgt_data[station_number] = {
+            "yearly": yearly[0],
+            "monthly": {m[0]: m[1] for m in monthly},
+        }
+        print(f"Loaded station {station_number}")
+
+    return wbgt_data
+
+
 def main():
     with open(processed_dir.joinpath("daily_weather.json"), "w") as f:
         json.dump(create_daily_weather_object(), f, indent=2)
 
     with open(processed_dir.joinpath("monthly_yearly_weather.json"), "w") as f:
         json.dump(create_monthly_yearly_weather_object(), f, indent=2)
+
+    with open(processed_dir.joinpath("daily_wbgt.json"), "w") as f:
+        json.dump(create_daily_wbgt_object(), f, indent=2)
+
+    with open(processed_dir.joinpath("monthly_yearly_wbgt.json"), "w") as f:
+        json.dump(create_monthly_yearly_wbgt_object(), f, indent=2)
 
 
 if __name__ == "__main__":
