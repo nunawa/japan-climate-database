83d3f0d0d3a3819a853215c2cea05011e474bdb5
nunawa <71294849+nunawa@users.noreply.github.com>
2025-09-22 01:25:39 +0900

    Rename weather* to normal* for JMA normals

--- data-processing/main.py
+++ data-processing/main.py
@@ -56,7 +56,7 @@ def convert_all_none_to_none(x: tuple | list):
         return x
 
 
-def create_daily_weather_object():
+def create_daily_normal_object():
     csv_files = jma_normal_dir.joinpath("daily").glob("nml_amd_d_*.csv")
 
     column_names = [
@@ -73,7 +73,7 @@ def create_daily_weather_object():
         column_names.append(f"day{day}_value")
         column_names.append(f"day{day}_remark")
 
-    weather_data = {}
+    normal_data = {}
 
     for csv in csv_files:
         daily = duckdb.read_csv(csv, names=column_names).set_alias("daily")
@@ -100,17 +100,17 @@ def create_daily_weather_object():
             sunshine_duration = None
 
         station_number = csv.stem.split("_")[-1]
-        weather_data[station_number] = {
+        normal_data[station_number] = {
             "temperature": temperature,
             "precipitation": precipitation,
             "sunshine_duration": sunshine_duration,
         }
         print(f"Loaded station {station_number}")
 
-    return weather_data
+    return normal_data
 
 
-def create_monthly_yearly_weather_object():
+def create_monthly_yearly_normal_object():
     csv_files = jma_normal_dir.joinpath("monthly").glob("nml_amd_m_*.csv")
 
     column_names = [
@@ -129,7 +129,7 @@ def create_monthly_yearly_weather_object():
     column_names.append("year_value")
     column_names.append("year_remark")
 
-    weather_data = {}
+    normal_data = {}
 
     for csv in csv_files:
         monthly = duckdb.read_csv(csv, names=column_names).set_alias("monthly")
@@ -139,7 +139,7 @@ def create_monthly_yearly_weather_object():
         raw_sunshine_duration = query_monthly_yearly(monthly, "3500").fetchall()
 
         station_number = csv.stem.split("_")[-1]
-        weather_data[station_number] = {
+        normal_data[station_number] = {
             "monthly": {
                 "temperature": convert_all_none_to_none(raw_temperature[0][1:-2]),
                 "precipitation": convert_all_none_to_none(raw_precipitation[0][1:-2]),
@@ -155,7 +155,7 @@ def create_monthly_yearly_weather_object():
         }
         print(f"Loaded station {station_number}")
 
-    return weather_data
+    return normal_data
 
 
 def create_daily_wbgt_object():
@@ -267,11 +267,11 @@ def create_monthly_yearly_wbgt_object():
 
 
 def main():
-    with open(processed_dir.joinpath("daily_weather.json"), "w") as f:
-        json.dump(create_daily_weather_object(), f, indent=2)
+    with open(processed_dir.joinpath("daily_normal.json"), "w") as f:
+        json.dump(create_daily_normal_object(), f, indent=2)
 
-    with open(processed_dir.joinpath("monthly_yearly_weather.json"), "w") as f:
-        json.dump(create_monthly_yearly_weather_object(), f, indent=2)
+    with open(processed_dir.joinpath("monthly_yearly_normal.json"), "w") as f:
+        json.dump(create_monthly_yearly_normal_object(), f, indent=2)
 
     with open(processed_dir.joinpath("daily_wbgt.json"), "w") as f:
         json.dump(create_daily_wbgt_object(), f, indent=2)
