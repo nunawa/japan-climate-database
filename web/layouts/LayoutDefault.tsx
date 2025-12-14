8c4178deb3caecda42e267c9fdbd3978fde0c8b7
nunawa <71294849+nunawa@users.noreply.github.com>
2025-12-14 19:54:10 +0900

    Update Navbar to valid contents

--- web/layouts/LayoutDefault.tsx
+++ web/layouts/LayoutDefault.tsx
@@ -1,7 +1,7 @@
 import "@mantine/core/styles.css";
-import '@mantine/charts/styles.css';
-import "mantine-datatable/styles.layer.css"
-import "maplibre-gl/dist/maplibre-gl.css"
+import "@mantine/charts/styles.css";
+import "mantine-datatable/styles.layer.css";
+import "maplibre-gl/dist/maplibre-gl.css";
 
 import logoUrl from "../assets/logo.svg";
 import { AppShell, Burger, Group, Image, MantineProvider } from "@mantine/core";
@@ -9,18 +9,31 @@ import { useDisclosure } from "@mantine/hooks";
 import { Link } from "../components/Link";
 import theme from "./theme.js";
 
-export default function LayoutDefault({ children }: { children: React.ReactNode }) {
-  const [opened, { toggle }] = useDisclosure();
+export default function LayoutDefault({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [opened, { toggle }] = useDisclosure(false);
   return (
     <MantineProvider theme={theme}>
       <AppShell
         header={{ height: 60 }}
-        navbar={{ width: 300, breakpoint: "sm", collapsed: { mobile: !opened } }}
+        navbar={{
+          width: 300,
+          breakpoint: "sm",
+          collapsed: { mobile: !opened },
+        }}
         padding="md"
       >
         <AppShell.Header>
           <Group h="100%" px="md">
-            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              hiddenFrom="sm"
+              size="sm"
+            />
             <a href="/">
               {" "}
               <Image h={50} fit="contain" src={logoUrl} />{" "}
@@ -28,9 +41,9 @@ export default function LayoutDefault({ children }: { children: React.ReactNode 
           </Group>
         </AppShell.Header>
         <AppShell.Navbar p="md">
-          <Link href="/" label="Welcome" />
-          <Link href="/todo" label="Todo" />
-          <Link href="/star-wars" label="Data Fetching" />
+          <Link href="/" label="ホーム" />
+          <Link href="/map" label="マップ" />
+          <Link href="/ranking" label="ランキング" />
         </AppShell.Navbar>
         <AppShell.Main> {children} </AppShell.Main>
       </AppShell>
